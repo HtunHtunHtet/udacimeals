1c19d26dd5d9bf2930af9345901ac5ba5b424b64
htunhtunhtet <ryanhhh91@gmail.com>
2018-02-13 15:28:30 +0800

    add index

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const REMOVE_FROM_CALENDER = 'REMOVE_FROM_CALENDER'
+
+export function addRecipe({day, recipe, meal}) {
+    return {
+        type : ADD_RECIPE,
+        day, recipe, meal
+    }
+}
+
+export function removeFromCalender ({recipe, meal}){
+    return {
+        type: REMOVE_FROM_CALENDER,
+        recipe, meal
+    }
+}
